e6f7ba6e04304d6697274e10196b5ba1c1095f7e
Ashley Twomey <aprier091012@gmail.com>
2019-09-21 01:04:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,7 @@
 def prime?(number)
-  if [number].any?{|i| i > number}
-    return true
-  elsif 
-  number -= 1
-  return false 
-else 
-  [number].any?{|i| i > number}
-  return true
+  if  number -= 1
+  return true  
+ else [number].any?{|i| i > number}
+  return false
 end
 end
