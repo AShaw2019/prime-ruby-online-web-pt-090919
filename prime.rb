51715cdbee7b9b77986051a4940bb289897b7975
Ashley Twomey <aprier091012@gmail.com>
2019-09-21 01:19:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,10 @@
-def prime?(number)
-  if [number].any?{|i| i > number}
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n <= 3
     return true
-  else
-    
- end
+  else (2..n/2).none? do |x|
+    n % x == 0
+  end
+  end
+end
