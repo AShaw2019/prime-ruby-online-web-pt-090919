975c63d2fee0e437e0c6909ec05e1bd8e1bd8da6
Ashley Twomey <aprier091012@gmail.com>
2019-09-21 01:09:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,10 @@ def prime?(number)
   if [number].any?{|i| i > number}
     return true
   else 
-  if 
+  if all_odd = true
+[1,2,3].each do |number|
+  if number.even?
+    
   return false
  end
  end
