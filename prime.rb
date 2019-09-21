52d747161360170534c1529aed4375617284e5b3
Ashley Twomey <aprier091012@gmail.com>
2019-09-21 01:11:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(number)
   if all_prime = true
 [1,2,3].each do |number|
   if number.non_prime?
-    all_odd = false
+    all_prime = false
   else
   [number].any?{|i| i > number}
     return true
